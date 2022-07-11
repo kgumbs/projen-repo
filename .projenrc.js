853cfed7aabf3569dd1c07f499da9b856f4d7383
Mark Washburn <mark@mrwconsulting.tech>
2022-07-11 12:54:53 -0500

    chore: code cleanup

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,15 @@
 const { awscdk } = require('projen');
+const { NpmAccess } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'kgumbs',
   authorAddress: 'kwesigumbs@hotmail.com',
   cdkVersion: '2.31.0',
   defaultReleaseBranch: 'main',
+  majorVersion: 1,
   name: '@kgumbs/projen-repo',
   packageName: '@kgumbs/projen-repo',
   repositoryUrl: 'https://github.com/kgumbs/projen-repo.git',
+  npmAccess: NpmAccess.PUBLIC,
   npmRegistryUrl: 'https://npm.pkg.github.com',
   releaseToNpm: true,
 });
